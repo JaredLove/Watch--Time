a3ee2f636dec6c51855abd51c64433ddef61e42f
Jared Love <lovejared91@yahoo.com>
2022-12-14 20:19:23 -0600

    heroku

--- server/server.js
+++ server/server.js
@@ -21,20 +21,24 @@ const server = new ApolloServer({
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }  
+  
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+  
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   // integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
   
   // Serve up static assets
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-  }
+
   app.use(routes);
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
+
   
   db.once('open', () => {
       app.listen(PORT, () => {
