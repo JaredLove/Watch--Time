4683a3dc5f68e03b4f057b0dcca0d61251cc6a65
Jared Love <lovejared91@yahoo.com>
2022-12-20 12:52:11 -0600

    login

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -11,21 +11,13 @@ const {
 const { authMiddleware } = require("../../utils/auth");
 
 // put authMiddleware anywhere we need to send a token for verification of user
-<<<<<<< HEAD
-// router.route("/").post(createUser).put(authMiddleware);
-=======
 router.route('/').post(createUser).put(authMiddleware, saveMovie);
-
-router.route('/').post(createUser).put(authMiddleware);
->>>>>>> 0e408245d56d3dc404aae2a4f8ae738fee1eb101
 
 router.route("/login").post(login);
 
-<<<<<<< HEAD
-router.route("/me").get(authMiddleware, getSingleUser);
-=======
+router.route('/me').get(authMiddleware, getSingleUser);
+
 router.route('/movies/:movieId').delete(authMiddleware, deleteMovie);
 
->>>>>>> 0e408245d56d3dc404aae2a4f8ae738fee1eb101
 
 module.exports = router;
